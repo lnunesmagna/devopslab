04a2763e2152738f87eed3fcb002acbfc071673c
lnunesmagna <105680311+lnunesmagna@users.noreply.github.com>
2022-05-28 11:07:12 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Grupo 3 - MBA FIAP - Arquitetura de Soluções"
+    return "Grupo 3 - MBA FIAP - Arquitetura de Soluçõess"
 
 if __name__ == '__main__':
     app.run()
